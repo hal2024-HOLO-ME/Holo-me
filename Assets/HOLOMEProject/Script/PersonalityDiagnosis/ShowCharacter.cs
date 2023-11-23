1e5e840baa7c3768acb976f544c3effe658ccc09
YusukeTeo <siroha323@gmail.com>
2023-11-23 14:33:20 +0900

    add: キャラクターを表示する

---
+++ Assets/HOLOMEProject/Script/PersonalityDiagnosis/ShowCharacter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCharacter : MonoBehaviour
+{
+    private string gameObjectName;
+    public GameObject personalityDiagnosisResultSlate;
+
+    /// <summary>
+    /// 「会いに行く」ボタンをクリック時に発火
+    /// Resorces/を対象にDBから取得したModelファイル名と一致するモデルを探し、生成する
+    /// 性格診断結果表示パネルを隠す
+    /// </summary>
+    public void OnMeetButtonClicked()
+    {
+        // HACK： Static変数をGetterで拾っている
+        gameObjectName = new SendResult().GetResponseFileName();
+        GameObject generateObject = GameObject.Find("GenerateObject");
+        // fbxLoaderのコンストラクタを実行して、generateObjectにMiiを生成する。
+        FbxLoader fbxLoader = generateObject.GetComponent<FbxLoader>();
+
+        personalityDiagnosisResultSlate.SetActive(false);
+        fbxLoader.SetGameObjectName(gameObjectName);
+        fbxLoader.GenerateObject();
+    }
+}
